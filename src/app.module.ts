2745495a6e9ac377737e19a84ce4b21881231b25
Jonathan Luego <jonathan.lugo@descanso.local>
2026-04-16 15:37:10 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TasksController } from './tasks/tasks.controller';
+
+@Module({
+  imports: [TasksModule],
+
+})
+export class AppModule {}
